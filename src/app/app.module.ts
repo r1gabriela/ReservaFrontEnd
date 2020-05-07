6d7934746d5b3f6dd03c6d5c86eddfebafcf4e1c
devMarcos- <marcosgrsouto@gmail.com>
2020-05-07 01:30:00 -0300

    TASK#92769-71: Implementar Tela de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { routing } from './app.routing';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    routing,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
