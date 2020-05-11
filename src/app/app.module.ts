dce934bb837233d6dc1126d493eafe262e38eefa
Isaac_andre <andreisaac344@gmail.com>
2020-05-11 01:52:50 -0300

    TASK#92769-83: Implementar Tela de Manter Data Comemorativa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,24 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CalendarModule} from 'primeng/calendar';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {DropdownModule} from 'primeng/dropdown';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { MenuComponent } from './menu/menu.component';
+import { ManterDataComemoracaoComponent } from './manter-data-comemoracao/manter-data-comemoracao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CadastroComponent,
-    MenuComponent
+    MenuComponent,
+    ManterDataComemoracaoComponent
+    
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,11 @@ import { MenuComponent } from './menu/menu.component';
     InputTextModule,
     ButtonModule,
     BrowserAnimationsModule,
-    PanelMenuModule
+    PanelMenuModule,
+    CalendarModule,
+    AutoCompleteModule,
+    DropdownModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ManterDataComemoracaoComponent } from './manter-data-comemoracao/manter-data-comemoracao.component';
 
 const APP_ROUTES: Routes = [
   {
@@ -12,7 +13,12 @@ const APP_ROUTES: Routes = [
   {
     path: 'cadastro',
     component: CadastroComponent
+  },
+  {
+    path: 'dataComemorativa/manter',
+    component: ManterDataComemoracaoComponent
   }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
