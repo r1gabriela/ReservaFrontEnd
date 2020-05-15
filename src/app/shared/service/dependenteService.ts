00a2b8178319f4f989060d6e26167807b927cbd2
unknown <erick.jose634@gmail.com>
2020-05-15 11:52:21 -0300

    TASK#92769-126: Implementar Service do Crud Dependente

---
+++ src/app/shared/service/dependenteService.ts
@@ -0,0 +1,23 @@
+import { Dependente } from './../dependente';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DependenteService {
+
+  private SALVAR = 'http://localhost:8080/restaurante/rest/dependente/salvar';
+  private EXCLUIR = 'http://localhost:8080/restaurante/rest/dependente/excluir';
+
+  constructor(private httpClient: HttpClient, private httpParams: HttpParams) { }
+
+  salvar(dependente: Dependente){
+    return this.httpClient.post<Dependente>(this.SALVAR, dependente);
+  }
+
+  excluir(dependente: Dependente){
+    return this.httpClient.post<Boolean>(this.EXCLUIR, dependente);
+  }
+
+}
